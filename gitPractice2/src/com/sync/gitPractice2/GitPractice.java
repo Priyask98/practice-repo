ff2ee24f93ff61d8a811a85e94868a8988dc1a8e
priya <priyankaskalasappanavr@gmail.com>
2022-11-11 12:35:32 +0530

    1st commit

---
+++ gitPractice2/src/com/sync/gitPractice2/GitPractice.java
@@ -0,0 +1,10 @@
+package com.sync.gitPractice2;
+
+public class GitPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to synchronoss.....1st push");
+	}
+
+}
